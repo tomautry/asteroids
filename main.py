c86658d0ec65a35fb096938df8d6aa3904e7febd
Tom Autry <tomuhtree@gmail.com>
2026-03-29 20:21:07 -0500

    worked on some stuff

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
         pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        milliseconds = clock.tick(60)
+        dt = milliseconds / 1000.0
+        player.draw(screen)
 
 
 if __name__ == "__main__":
